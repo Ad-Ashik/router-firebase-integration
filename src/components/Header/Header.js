d04884db86516f1d7ad304c26964b80070e5efa1
AIA <adashik585@gmail.com>
2022-04-09 11:34:52 +0600

    user firebase coustom hook

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
+import { getAuth, signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
-import useFirebase from '../../hooks/useFirebase';
-import './Header.css'
+import app from '../../firebase.init';
+import './Header.css';
+
+const auth = getAuth(app);
 
 const Header = () => {
-    const { user, singOut } = useFirebase();
+    const [user] = useAuthState(auth)
     return (
         <div className='header'>
             <nav>
@@ -17,7 +21,7 @@ const Header = () => {
                 {
 
                     user?.uid ?
-                        <button onClick={singOut}>Log Out</button> :
+                        <button onClick={() => signOut(auth)}>Log Out</button> :
                         <Link to="/login">Login</Link>
                 }
             </nav>
