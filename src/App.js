72ad4a1bfac7fc812781213fc9a7fc13ca892d94
AIA <adashik585@gmail.com>
2022-04-09 11:58:45 +0600

    react router requer auth

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
+import Order from './components/Order/Order';
 import Register from './components/Register/Register';
+import RequerAuth from './components/RequerAuth/RequerAuth';
 
 function App() {
   return (
@@ -13,6 +15,11 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='/orders' element={
+          <RequerAuth>
+            <Order></Order>
+          </RequerAuth>
+        }></Route>
       </Routes>
     </div>
   );
